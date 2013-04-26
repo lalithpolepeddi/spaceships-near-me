065c11cccfd44c1113bb147c0db8245e8d0c6ac9
cuttarug <lpolepeddi@gmail.com>
2013-04-26 15:38:30 -0600

    Fullscreen map

--- www/js/index.js
+++ www/js/index.js
@@ -17,33 +17,33 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  initialize: function() {
+    this.bindEvents();
+  },
+  
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  
+  map: L.map('map'),
+  
+  onDeviceReady: function() {
+    app.initializeMap();
+    navigator.geolocation.getCurrentPosition(app.onLocationFound, app.onLocationError)
+  },
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+  initializeMap: function() {
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      maxZoom: 18
+    }).addTo(app.map);
+  },
 
-        console.log('Received Event: ' + id);
-    }
+  onLocationFound: function(position) {
+    app.map.setView([position.coords.latitude, position.coords.longitude], 14);
+  },
+
+  onLocationError: function(error) {
+    app.map.setView([37.7749295,-122.4194155], 14);
+  }
 };
